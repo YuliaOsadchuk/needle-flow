4a5c78e86dc00ab61e2e6959819b3ac0cd7e88f2
yulia <yulia.osadchuk@gmail.com>
2026-06-05 18:48:31 +0300

    initial commit

---
+++ src/main/java/yosadchuk/needle/flow/NeedleFlowApplication.java
@@ -0,0 +1,13 @@
+package yosadchuk.needle.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedleFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedleFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yosadchuk/needle/flow/NeedleFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package yosadchuk.needle.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedleFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
